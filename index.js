a6f746d48377d609091daa834bb1915c54f789d2
Jonas Faber <jonas.faber@iem.thm.de>
2016-01-25 22:22:45 +0100

    First Try with MQTT Client

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+var config = require('./config.json');
 var mqtt    = require('mqtt');
-var client  = mqtt.connect('mqtt://192.168.178.21');
+var client  = mqtt.connect('mqtt://'+config.ip);
 
 client.on('connect', function () {
-  client.subscribe('presence');
-  client.publish('presence', 'Hello mqtt');
+  client.subscribe(config.channel+"-out");
 });
 
 client.on('message', function (topic, message) {
   // message is Buffer
+  switch (message) {
+    case "off":
+      //Turn off PC here.
+      console.log("Turning off PC.");
+      break;
+    case "get":
+      client.publish(config.channel+"-in","on");
+      break;
+    default:
+      break;
+  }
   console.log(message.toString());
-  client.end();
 });
