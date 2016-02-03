f9e3740c29b93073ca8f859e393e23692d4cd60f
Jonas Faber <jonas.faber@iem.thm.de>
2016-02-03 02:05:38 +0100

    Add actual functuallity to restart pc on command. Add more Logging.

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 var config = require('./config.json');
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://'+config.ip);
+var shell = require('shelljs');
 
 client.on('connect', function () {
+  console.log("Connected to MQTT broker");
   client.subscribe(config.channel+"-out");
+  console.log("Subscribed to "+config.channel+"-out");
 });
 
 client.on('message', function (topic, message) {
   // message is Buffer
-  switch (message) {
+  switch (message.toString()) {
     case "off":
       //Turn off PC here.
       console.log("Turning off PC.");
+      shell.exec('shutdown /s /t 120');
       break;
     case "get":
       client.publish(config.channel+"-in","on");
+      console.log("Request: Is PC on?");
+      console.log("Answer: Yes!");
       break;
     default:
-      break;
+      console.log(message.toString());
   }
-  console.log(message.toString());
+
 });
